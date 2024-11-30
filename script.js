9d4db8aab010c1f61ca394351b4d5111659fbe88
Sakshi9980 <141515536+Sakshi9980@users.noreply.github.com>
2024-12-01 00:14:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 document.getElementById("download-tool").addEventListener("click", () => {
-    window.location.href = "https://github.com/Sakshi9980/virtualmouse/releases/download/virtual/mysetup.exe
-"; // Replace <username> and <repo-name>
+    window.location.href = "https://github.com/Sakshi9980/virtualmouse/releases/download/virtual/mysetup.exe"; // Correct URL
 });
