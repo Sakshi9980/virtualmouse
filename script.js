7673db05a1f8d36af179308ef6e02d82ea8b07fe
Sakshi9980 <141515536+Sakshi9980@users.noreply.github.com>
2024-11-30 22:09:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById("download-tool").addEventListener("click", () => {
+    window.location.href = "D://MAJOR//TheVirtualMouseProject//dist//VirtualMouse.exe"; // Change path if needed
+});
+
+document.getElementById("install-extension").addEventListener("click", () => {
+    window.open("https://chrome.google.com/webstore/detail/virtual-mouse-extension", "_blank");
+});
