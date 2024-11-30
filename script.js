84ccabd402d2c4e7f2da105be64ad0741cb4639a
Sakshi9980 <141515536+Sakshi9980@users.noreply.github.com>
2024-11-30 22:27:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-
 document.getElementById("download-tool").addEventListener("click", () => {
-    window.location.href = "https://sakshi9980.github.io/virtualmouse/VirtualMouse.exe"; // Change path if needed
-});
-
-document.getElementById("install-extension").addEventListener("click", () => {
-    window.open("https://chrome.google.com/webstore/detail/virtual-mouse-extension", "_blank");
+    window.location.href = "https://sakshi9980.github.io/virtualmouse/VirtualMouse.exe"; // Replace <username> and <repo-name>
 });
