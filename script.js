bbcec831a8dd74d24288100864de1dfb726edfef
Sakshi9980 <141515536+Sakshi9980@users.noreply.github.com>
2024-11-30 22:25:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 document.getElementById("download-tool").addEventListener("click", () => {
-    window.location.href = "D://MAJOR//TheVirtualMouseProject//dist//VirtualMouse.exe"; // Change path if needed
+    window.location.href = "https://sakshi9980.github.io/virtualmouse/VirtualMouse.exe"; // Change path if needed
 });
 
 document.getElementById("install-extension").addEventListener("click", () => {
